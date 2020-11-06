f63d8d6083be3b446bc212df25b2a51f41f20036
Rafael Alves <rafael.rac.mg@gmail.com>
2020-11-06 20:02:58 -0300

    test: signin infrastructure

--- 1.api_express_sequelize/src/signin/infrastructure/hash/hash.compare.ts
+++ 1.api_express_sequelize/src/signin/infrastructure/hash/hash.compare.ts
@@ -1,15 +1,12 @@
 import bcrypt from 'bcryptjs'
-import { promisify } from 'util'
 
 import { Either, left, right, BaseError } from '../../../core/business'
 import { IHashCompare } from '../../application'
 
 export class HashCompare implements IHashCompare {
   public async compare (original: string, hash: string): Promise<Either<BaseError, boolean>> {
-    const toPromise = promisify(bcrypt.compare)
-
     try {
-      const result = await toPromise(original, hash)
+      const result = await bcrypt.compare(original, hash)
       return right(result)
     } catch (err) {
       console.log('SIGNIN MODULE - HASH COMPARE ERR', err)
